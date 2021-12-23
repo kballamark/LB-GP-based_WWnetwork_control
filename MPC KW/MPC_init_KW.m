5dcfe194d6eab4c650eb505e979ae7453160bca2
Krisztian Balla <cballa7@gmail.com>
2021-12-23 15:15:30 +0100

    KW recreate Simulink file

--- MPC KW/MPC_init_KW.m
+++ MPC KW/MPC_init_KW.m
@@ -2,11 +2,9 @@
 clear all;
 clc;
 %% ============================================ Control setup ======================================
-addpath('.\Lab_simulator\Simulator\lab signal design\generated_data');
-load('.\Lab_simulator\Simulator\lab signal design\generated_data\d_lab');
-addpath('.\Lab_simulator\Simulator')
-
-dataLoad;
+% addpath('.\Lab_simulator\Simulator\lab signal design\generated_data');
+% load('.\Lab_simulator\Simulator\lab signal design\generated_data\d_lab');
+% addpath('.\Lab_simulator\Simulator')
 
 %% Basic properties for simulator
 Nxt = 2;                            % number of tank states
@@ -17,23 +15,24 @@ ND = 3;                             % number of disturbances
 load('.\Lab_simulator\Simulator\parameters\P_pipe_min')     % load Gravity pipe parameters
 load('.\Lab_simulator\Simulator\parameters\P_pipe_min_v2')     % load Gravity pipe parameters
 
+Kt = 4.9087;
 P_sim = [P_pipe_min_v2, Kt, Kt]';      % all sim parameters
 
 %% Properties for ON/OFF controller
 % Tank constraints
-max_t1 = 7.02;     
+max_t1 = 7.9;     
 min_t1 = 1.7;             % 6.7 physical maximum 
-max_t2 = 6.5;%7.02;     
+max_t2 = 6.4;%7.02;     
 min_t2 = 1.7;
 
 % Pipe constraints
 h_p_max = [0.3;0.3;0.3;0.3];
 h_p_min = [0.0001;0.0001;0.0001;0.0001];
 
-u1_on = 8.3;          
-u1_off = 3.4;
-u2_on = 18.5;%17.5;%19.5;         
-u2_off = 5.4;
+u1_on = 10.5;          
+u1_off = 5;
+u2_on = 20;%17.5;%19.5;         
+u2_off = 6;
 
 %% MPC specs
 Hp = 40;
@@ -56,6 +55,8 @@ X_ref_sim_mod = X_ref_sim;
 clear X_ref_sim;
 X_ref_sim(1,:) = resample(X_ref_sim_mod(1,:),1,2);
 X_ref_sim(2,:) = resample(X_ref_sim_mod(2,:),1,2);
+
+D_sim(3,:) = D_sim(3,:)*0.8+1.5;
 
 %%
 MPC_builder;
