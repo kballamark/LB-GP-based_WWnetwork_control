cf02c5ecf4e10df49c0b50cf886538ee28182ef3
Krisztian Balla <cballa7@gmail.com>
2021-12-29 19:32:32 +0100

    Simulink KW-MPC template refinement

--- MPC KW/MPC_init_KW.m
+++ MPC KW/MPC_init_KW.m
@@ -19,10 +19,10 @@ P_sim = [P_pipe_min_v2, Kt, Kt]';      % all sim parameters
 
 %% Properties for ON/OFF controller
 % Tank constraints
-max_t1 = 7.9;     
-min_t1 = 1.7;             % 6.7 physical maximum 
+max_t1 = 6.9;     
+min_t1 = 2;             % 6.7 physical maximum 
 max_t2 = 6.4;%7.02;     
-min_t2 = 1.7;
+min_t2 = 2;
 
 % Pipe constraints
 h_p_max = [0.3;0.3;0.3;0.3];
