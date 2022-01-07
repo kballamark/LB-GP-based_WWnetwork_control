cff354a152e15b38c36df6d12f0ec277f6923499
Krisztian Balla <cballa7@gmail.com>
2022-01-07 17:47:53 +0100

    Reinit lab for new GP-MPC test

--- Data loader/dataLoad.m
+++ Data loader/dataLoad.m
@@ -15,7 +15,7 @@ clear all; clc
 % """
 
 addpath('data');
-load('data/DataSave_Onoff_1_to_2800');
+load('data/DataSave_Onoff_2500_to_5800');
 %load('Lab_simulator\Simulator\data\Simulation_data_WWdata_v2');
 %%
 labRes = ans;
