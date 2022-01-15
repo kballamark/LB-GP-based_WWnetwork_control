1ae4f1142c611c2a6a74046cf9308a904b3ee92a
Krisztian Balla <cballa7@gmail.com>
2022-01-15 11:57:22 +0100

    Long ON/OFF experiment

--- RTC-Onoff/Initialize_onoff.m
+++ RTC-Onoff/Initialize_onoff.m
@@ -12,18 +12,18 @@ ND = 3;                             % number of disturbances
 %% Properties - controller
 % Tank constraints
 max_t1 = 6.8;           % 6.7 physical maximum    
-min_t1 = 3.8;             % 6.7 physical maximum 
+min_t1 = 2;%3.8;             % 6.7 physical maximum 
 max_t2 = 6.1;           %6.1 max?      
-min_t2 = 4.3;
+min_t2 = 2;%4.3;
 
 % Pipe constraints
 h_p_max = [0.3;0.3;0.3;0.3];
 h_p_min = [0.0001;0.0001;0.0001;0.0001];
 
 % Pump constraints
-u1_on = 6.5;%10.5;          
+u1_on = 8;%6.5;         
 u1_off = 3.5;
-u2_on = 14;%17.5;%19.5;         
+u2_on = 19;%14;         
 u2_off = 5.4;
 
 %% Control specs
