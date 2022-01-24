0fa08b0dfe3c1effcc92312b79b54d0c05619f42
Krisztian Balla <cballa7@gmail.com>
2022-01-24 08:54:29 +0100

    DataSave_24_01_2022

--- Data loader/dataLoad.m
+++ Data loader/dataLoad.m
@@ -15,7 +15,7 @@ clear all; clc
 % """
 
 addpath('data');
-load('data/DataSave_MPC_GP_23_01_2022_v1');
+load('data/DataSave_MPC_GP_24_01_2022');
 %load('Lab_simulator\Simulator\data\Simulation_data_WWdata_v2');
 %%
 labRes = ans;
