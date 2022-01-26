fe39b0e46506782cd19a6f7b6437d6ba686778f7
Krisztian Balla <cballa7@gmail.com>
2022-01-26 19:00:56 +0100

    DataSave_26_01_2022 Onoff

--- Data loader/Plots/KPIs/KPI_plot.m
+++ Data loader/Plots/KPIs/KPI_plot.m
@@ -39,23 +39,23 @@ yyaxis left
 %
 a = [1002-startPlot 1017-startPlot 1017-startPlot 1002-startPlot];
 b = [0 0 1.8 1.8];
-patch(a,b,'yellow','FaceAlpha',.4,'LineStyle',':')
+patch(a,b,'black','FaceAlpha',.2,'LineStyle',':','HandleVisibility','off')
 hold on
 a = [1727-startPlot 1739-startPlot 1739-startPlot 1727-startPlot];
 b = [0 0 1.8 1.8];
-patch(a,b,'yellow','FaceAlpha',.4,'LineStyle',':')
+patch(a,b,'black','FaceAlpha',.2,'LineStyle',':','HandleVisibility','off')
 hold on
 a = [1811-startPlot 1825-startPlot 1825-startPlot 1811-startPlot];
 b = [0 0 1.8 1.8];
-patch(a,b,'yellow','FaceAlpha',.4,'LineStyle',':')
+patch(a,b,'black','FaceAlpha',.2,'LineStyle',':','HandleVisibility','off')
 hold on
 a = [1855-startPlot 1862-startPlot 1862-startPlot 1855-startPlot];
 b = [0 0 1.8 1.8];
-patch(a,b,'yellow','FaceAlpha',.4,'LineStyle',':')
+patch(a,b,'black','FaceAlpha',.2,'LineStyle',':','HandleVisibility','off')
 hold on
 a = [2078-startPlot 2092-startPlot 2092-startPlot 2078-startPlot];
 b = [0 0 1.8 1.8];
-patch(a,b,'yellow','FaceAlpha',.4,'LineStyle',':')
+patch(a,b,'black','FaceAlpha',.2,'LineStyle',':')
 hold on
 %
 plot(KPI_s(startPlot:endPlot),'LineWidth',1,'Color',[0 0.2470 0.7410])
@@ -72,12 +72,11 @@ grid on
 % ylim([4,11])
 xlim([startPlot, length(KPI_u(startPlot:endPlot))]);
 xticks(103:115:length(KPI_u(startPlot:endPlot)))
-% leg = legend('Inflow');
-% set(leg,'Interpreter','latex');
+ leg = legend('Overflow','Safety KPI','Overflow KPI');
+ set(leg,'Interpreter','latex');
 h1.YAxis(1).Color = [0 0.2470 0.7410];
 h1.YAxis(2).Color = [1 0 0];
 set(gca,'xticklabel',[])
-
 
 
 ax(3) = subplot(3,1,3);
@@ -88,14 +87,17 @@ ylim([0,0.05])
 h2 = gca;
 yyaxis right
 bar(d_r(1,startPlot+5:endPlot),'FaceColor',[0.9290 0.6940 0.1250])
+ylabel('Flow ($\frac{\textrm{dm}^3}{\textrm{min}}$)','interpreter','latex');
 set(h2, 'YDir', 'reverse')
 ylim([0,5])
 title('(c) Uncertainty KPI','interpreter','latex')
 grid on
 xlim([startPlot, length(KPI_u(startPlot:endPlot))]);
 xticks(103:115:length(KPI_u(startPlot:endPlot)))
-% leg = legend('Inflow');
-% set(leg,'Interpreter','latex');
+h2.YAxis(1).Color = [0 0.2470 0.7410];
+h2.YAxis(2).Color = [0.9290 0.6940 0.1250];
+leg = legend('Uncertainty KPI','Rain forecast');
+set(leg,'Interpreter','latex');
 xlabel('Time (10s)','interpreter','latex');
 
 linkaxes(ax,'x');
